05ec86e8a4e63b0d0d0dfebc7224a9b435452fa4
ketang <ketang@virtualbox>
2020-12-15 19:41:41 +0800

    a

---
+++ bomb/bomb/c.c
@@ -0,0 +1,11 @@
+
+#include<iostream>
+#include<cstdio>
+using namespace std;
+int main(){
+	char* p = "0a";
+	char str[100]= "1 2 3 4 5 6";
+	int i1,i2,i3,i4,i5,i6;
+	sscanf(str,"%d %d %d %d %d %d", &i1,&i2,&i3,&i4,&i5,&i6);
+	
+	return 0;}
