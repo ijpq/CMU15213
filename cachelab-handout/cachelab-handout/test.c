8472cc38ab7c6a75822aa4f84481fca29fcc63db
ijpq <tangke0320@hotmail.com>
2020-12-30 18:53:54 +0800

    matrix first finished

--- cachelab-handout/cachelab-handout/test.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-	char c[5] = "0123";
-	c[1] = '3';
-	return 0;
-}
--- cachelab-handout/cachelab-handout/trans.c
+++ cachelab-handout/cachelab-handout/trans.c
@@ -10,7 +10,10 @@
 #include <stdio.h>
 #include "cachelab.h"
 
+#define MAGICSIZE 8
+
 int is_transpose(int M, int N, int A[N][M], int B[M][N]);
+void Optimize(int M, int N, int A[M][N], int B[M][N]);
 
 /* 
  * transpose_submit - This is the solution transpose function that you
@@ -22,6 +25,7 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
+	Optimize(M,N,A,B);
 }
 
 /* 
@@ -46,6 +50,51 @@ void trans(int M, int N, int A[N][M], int B[M][N])
 
 }
 
+char OptimizeM32_desc[] = "Optimize for M32N32";
+void OptimizeM32(int M, int N, int A[N][M], int B[M][N]) {
+
+	int iLocal, jLocal, iMov, jMov;
+	int tmp;
+	for (iMov=0; iMov<N;iMov+=8 ) {
+		for (jMov=0;jMov<M;jMov+=8) {
+			for (iLocal=0;iLocal<MAGICSIZE;iLocal++) {
+				for (jLocal=0;jLocal<MAGICSIZE;jLocal++) {
+					tmp = A[iLocal+iMov][jLocal+jMov];
+					B[jLocal+jMov][iLocal+iMov] = tmp;
+					
+				}
+			}
+		}
+	}
+	
+}
+
+char OptimizeM32_rowlocal_desc[] = "Optimize for 12 local vars";
+void OptimizeM32_rowlocal(int M, int N, int A[N][M], int B[M][N]) {
+	
+	int iLocal, jLocal, iMov, jMov;
+	int tmp8[8];
+	for (iMov=0; iMov<N;iMov+=8 ) {
+		for (jMov=0;jMov<M;jMov+=8) {
+			for (iLocal=0;iLocal<MAGICSIZE;iLocal++) {
+				for (jLocal=0;jLocal<MAGICSIZE;jLocal++) {
+					tmp8[jLocal] = A[iLocal+iMov][jLocal+jMov];	
+				}
+				for (jLocal=0;jLocal<MAGICSIZE;jLocal++) {
+					B[jLocal+jMov][iLocal+iMov] = tmp8[jLocal];
+				}
+				
+			}
+		}
+	}
+}
+
+
+void OptimizeM64(int M, int N, int A[N][M], int B[M][N]) {
+	//
+	
+}
+
 /*
  * registerFunctions - This function registers your transpose
  *     functions with the driver.  At runtime, the driver will
@@ -59,7 +108,11 @@ void registerFunctions()
     registerTransFunction(transpose_submit, transpose_submit_desc); 
 
     /* Register any additional transpose functions */
-    registerTransFunction(trans, trans_desc); 
+    //registerTransFunction(trans, trans_desc); 
+
+	registerTransFunction(OptimizeM32, OptimizeM32_desc);
+	registerTransFunction(OptimizeM32_rowlocal, OptimizeM32_rowlocal_desc);
+	registerTransFunction();
 
 }
 
